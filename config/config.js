83f5e6c2eaef72759f6cc07d3aab7063b94c7fe4
Izzyharvin <izzy84.ih@gmail.com>
2023-10-18 14:07:22 -0400

    css and video set up

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const config = {
     stripe: {
         secretKey: 'sk_test_51Nf6REK7KORITv4AHKsUxISFfGq03QomGZIkH9gjp3bxtLzqVd8WY1ZzF5WwA7ljiE11kwtPMYPs1rjYHyCL0yjZ009FlaE2cb',
         publishableKey: 'pk_test_51Nf6REK7KORITv4A9iGHDRV6tyE20MD5PfiaAg2iKkL1SatUYNKEf8tIddWsu3vpWaymGUcaFP4fSst9fHLvGVSP00TbmjBjYX',
+        webhookKey: 'whsec_06be1f70d7ccfa2590b97e0d6ab23118878d0fad0368a3b716b12486cc71b732',
     },
 };
 
--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             headers: {
                 'Accept': 'application/json',
             },
-        });        
+        });
 
         if (response.ok) {
             const data = await response.json();
@@ -31,53 +31,3 @@ document.addEventListener('DOMContentLoaded', async function () {
         console.error('Error fetching subscription status:', error);
     }
 });
-
-
-//document.addEventListener('DOMContentLoaded', async function () {
-//    const videoContainer = document.getElementById('video-container');
-//    const video = document.getElementById('subscription-video');
-
-//    try {
-//        const response = await fetch('/profile/check-subscription', {
-//            method: 'GET',
-//            headers: {
-//                'Accept': 'application/json',
-//            },
-//        });
-
-//        if (response.ok) {
-//            const data = await response.json();
-//            const isSubscribed = data.isSubscribed;
-
-//            if (isSubscribed) {
-//                // Load the subscribed user's video content
-//                video.src = '/videos/video.mp4';
-//                video.controls = true;
-//                videoContainer.style.display = 'block';
-//            } else {
-                // Display a message for non-subscribed users
-//                videoContainer.innerHTML = '<p>Sorry, this video is only available to subscribed users.</p>';
-//                videoContainer.style.display = 'block';
-//            }
-//        } else {
-//            console.error('Failed to fetch subscription status');
-//        }
-//    } catch (error) {
-//        console.error('Error fetching subscription status:', error);
-//    }
-//});
-
-// Assuming you have a global variable 'isSubscribed' that holds the subscription status
-// This variable can be set in your server-side route handler
-
-//document.addEventListener('DOMContentLoaded', function () {
-//    const videoContainer = document.getElementById('video-container');
-
-//    if (isSubscribed) {
-        // Load the subscribed user's video content
-//        videoContainer.innerHTML = '<iframe src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>';
-//    } else {
-        // Display a message or alternative content for non-subscribed users
-//        videoContainer.innerHTML = '<p>Sorry, this video is only available to subscribed users.</p>';
-//    }
-//});
